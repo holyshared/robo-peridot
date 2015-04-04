018997a12cb88149a0faf887c2d609723af86100
Noritaka Horio <holy.shared.design@gmail.com>
2015-04-04 17:47:09 +0900

    Upgrade expect plugin

--- peridot.php
+++ peridot.php
@@ -1,12 +1,12 @@
 <?php
 
 use Evenement\EventEmitterInterface;
-use expectation\peridot\ExpectationPlugin;
+use expect\peridot\ExpectPlugin;
 use cloak\peridot\CloakPlugin;
 
 return function(EventEmitterInterface $emitter)
 {
-    ExpectationPlugin::create()->registerTo($emitter);
+    ExpectPlugin::create()->registerTo($emitter);
 
     if (defined('HHVM_VERSION') === false) {
         CloakPlugin::create('.cloak.toml')->registerTo($emitter);
